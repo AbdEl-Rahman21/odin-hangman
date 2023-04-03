c4c21ef3def0d05b94f89987efc6792be9d086f9
AbdEl-Rahman21 <abdel292004@gmail.com>
2023-04-03 15:52:52 +0200

    Add save & load and rearrange functions

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'rainbow'
+require 'yaml'
 
 class Game
   def initialize
@@ -19,35 +20,13 @@ class Game
 
   private
 
-  attr_reader :word
-  attr_accessor :guesses, :blank_word, :remaining_guesses
+  attr_accessor :guesses, :blank_word, :remaining_guesses, :word
 
-  def intro
-    system('clear')
-
-    puts "\t\t== Open for business 3.0 =="
-    print Rainbow('[1]').color(:blue)
-    puts "\sPlay new game."
-    print Rainbow('[2]').color(:blue)
-    puts "\sLoad old game."
-  end
-
-  def layout
-    puts Rainbow("Remaining Guesses: #{remaining_guesses}").color(:yellow)
-
-    blank_word.each { |letter| print Rainbow(letter.to_s).color(:blue) }
-
-    print Rainbow("\tGuesses: ").color(:red)
-
-    guesses.each do |letter|
-      if word.include?(letter)
-        print "#{Rainbow(letter.to_s).color(:blue)}\s"
-      else
-        print "#{Rainbow(letter.to_s).color(:red)}\s"
-      end
-    end
-
-    print "\nEnter your guess or \"save\": "
+  def setup
+    @word = get_word.split('')
+    @guesses = []
+    @remaining_guesses = 10
+    @blank_word = Array.new(word.length, '_')
   end
 
   def get_word
@@ -57,40 +36,46 @@ class Game
       .sample
   end
 
-  def save_game; end
+  def intro
+    system('clear')
 
-  def load_game; end
+    puts "\t\t== Open for business 3.0 =="
+    puts "#{Rainbow('[1]').color(:blue)} Play new game."
+    puts "#{Rainbow('[2]').color(:blue)} Load old game."
 
-  def get_letter
+    load_game if get_choice == '2'
+  end
+
+  def get_choice
     loop do
-      choice = gets.chomp.downcase
+      choice = gets.chomp
 
-      if choice == 'save'
-        save_game
-      elsif invalid_input?(choice)
-        puts 'Error: Input must be 1 alphabetic character and not already guessed.'
-      else
-        return choice
-      end
+      return choice if %w[1 2].include?(choice)
+
+      puts 'Error: Invalid Input.'
     end
   end
 
-  def invalid_input?(choice)
-    if choice.length != 1 || !choice.match?(/[[:alpha:]]/) ||
-       guesses.include?(choice)
-      true
-    end
+  def load_game
+    puts Dir.glob('save_data/*.yaml')
+
+    save = YAML.safe_load File.read("save_data/#{get_save_to_load}.yaml")
+
+    self.word = save[:word]
+    self.guesses = save[:guesses]
+    self.remaining_guesses = save[:remaining_guesses]
+    self.blank_word = save[:blank_word]
   end
 
-  def play_turn
-    letter = get_letter
+  def get_save_to_load
+    loop do
+      print 'Enter save name: '
 
-    guesses.push(letter)
+      save_name = gets.chomp
 
-    if word.include?(letter)
-      word.each_with_index { |e, i| blank_word[i] = letter if e == letter }
-    else
-      self.remaining_guesses -= 1
+      return save_name if File.exist?("save_data/#{save_name}.yaml")
+
+      puts "Error:Save doesn't exist."
     end
   end
 
@@ -112,6 +97,101 @@ class Game
     end
   end
 
+  def play_turn
+    letter = get_letter
+
+    guesses.push(letter)
+
+    if word.include?(letter)
+      word.each_with_index { |e, i| blank_word[i] = letter if e == letter }
+    else
+      self.remaining_guesses -= 1
+    end
+  end
+
+  def layout
+    puts Rainbow("Remaining Guesses: #{remaining_guesses}").color(:yellow)
+
+    blank_word.each { |letter| print Rainbow(letter.to_s).color(:blue) }
+
+    print Rainbow("\tGuesses: ").color(:red)
+
+    guesses.each do |letter|
+      if word.include?(letter)
+        print "#{Rainbow(letter.to_s).color(:blue)}\s"
+      else
+        print "#{Rainbow(letter.to_s).color(:red)}\s"
+      end
+    end
+
+    print "\nEnter your guess or \"save\": "
+  end
+
+  def get_letter
+    loop do
+      choice = gets.chomp.downcase
+
+      if choice == 'save'
+        save_game
+      elsif invalid_input?(choice)
+        puts 'Error: Input must be 1 alphabetic character and not already guessed.'
+      else
+        return choice
+      end
+    end
+  end
+
+  def save_game
+    Dir.mkdir('save_data') unless Dir.exist?('save_data')
+
+    File.open("save_data/#{get_save_name}.yaml", 'w') do |file|
+      file.puts YAML.dump(
+        {
+          word: word,
+          guesses: guesses,
+          remaining_guesses: remaining_guesses,
+          blank_word: blank_word
+        }
+      )
+    end
+
+    exit
+  end
+
+  def get_save_name
+    loop do
+      print 'Enter save name: '
+
+      save_name = gets.chomp
+
+      return save_name unless File.exist?("save_data/#{save_name}.yaml")
+
+      print 'Name is taken do you want to override it (Y\\N): '
+
+      return save_name if override?
+    end
+  end
+
+  def override?
+    loop do
+      case gets.chomp.downcase
+      when 'y'
+        return true
+      when 'n'
+        return false
+      else
+        puts 'Error:Invalid Input.'
+      end
+    end
+  end
+
+  def invalid_input?(choice)
+    if choice.length != 1 || !choice.match?(/[[:alpha:]]/) ||
+       guesses.include?(choice)
+      true
+    end
+  end
+
   def repeat
     print Rainbow('[1]').color(:blue)
     puts "\sPlay again."
@@ -123,23 +203,6 @@ class Game
     else
       exit
     end
-  end
-
-  def get_choice
-    loop do
-      choice = gets.chomp
-
-      return choice if %w[1 2].include?(choice)
-
-      puts 'Error: Invalid Input.'
-    end
-  end
-
-  def setup
-    @word = get_word.split('')
-    @guesses = []
-    @remaining_guesses = 10
-    @blank_word = Array.new(word.length, '_')
   end
 end
 
